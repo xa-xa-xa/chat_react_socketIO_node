59c2f368df68311441fd426ac3fb68c2f7fe7975
Alexander Krasnorutskiy <alexk-mbp16@Alexanders-MBP.lan>
2020-02-26 11:31:33 -0800

    navbar higth

--- client_new/src/Components/Navbar/Navbar.js
+++ client_new/src/Components/Navbar/Navbar.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='absolute stick flex flex-row w-full h-6 bg-teal-800 text-white justify-around shadow-lg'>
+    <div className='absolute flex flex-row w-full h-8 bg-teal-800 text-white justify-around items-center shadow-lg'>
       <div className='ml-2'>RealTime Chat</div>
+
       <div className='text-teal-200'>Current topic:</div>
       <div className='text-teal-200'>Login</div>
       <div className='text-teal-200'>menu</div>
