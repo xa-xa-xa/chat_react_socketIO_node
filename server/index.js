11e4d1b6ce907e26f2cbf0d5c8641541f2a223ed
Alexander Krasnorutskiy <alexk-mbp16@Alexanders-MacBook-Pro.local>
2020-03-05 16:00:31 -0800

    refactor

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
 
+const { getUser, getUsersInRoom, addUser, removeUser } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -12,15 +14,39 @@ const server = http.createServer(app);
 // Socket
 const io = socket(server);
 io.on('connection', socket => {
-  console.log('we have new socket connection');
+  // console.log('we have new socket connection');
+
+  //* join user to conversation
   socket.on('join', ({ name, room }, callback) => {
     console.log('name:', name, '; room:', room);
+    //add user
+    const { error, user } = addUser({ id: socket.id, name, room });
+    if (error) {
+      callback(error);
+    }
 
-    // const error = true;
+    // notify user and others
+    // console.log(`${user.name}, welcome to the "${user.room}" chat.`);
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the "${user.room}" chat.`
+    });
+    socket.broadcast.to(user.room).emit('message', {
+      user: 'admin',
+      text: `${user.name}, has join the conversation.`
+    });
+    //join
+    socket.join(user.room);
 
-    // if (error) {
-    //   callback({ error: '! error' });
-    // }
+    callback();
+  });
+
+  //* sending messages
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
   });
 
   //disconnect
